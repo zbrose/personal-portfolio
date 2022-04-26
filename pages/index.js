7cd048542e19af9450f39d8f01fe57b317b70933
Zach Brose <97310057+zbrose@users.noreply.github.com>
2022-04-25 17:59:32 -0700

    updates bio

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
             <h1>About Me</h1>
             <p>I&apos;m a musician and software engineer living in Los Angeles, CA  </p>  
 
-            <p>I&apos;ve been a musician for over ten years now playing, producing and performing with a wide range of artists.  My favorite part of being a musician is collaborating with other artists to create something better than I could on my own while learning from them in the process.  After getting into software engineering, I realized how much these two fields have in common.  I see a team of engineers as a sort of musical group; playing, tinkering and even improvising to creatively and efficiently solve problems. </p>
+            <p>I&apos;ve been a musician for over ten years now playing, producing and performing with a wide range of artists.  My favorite part of being a musician is collaborating with other artists to create something better than I could on my own while learning from each other in the process.  After getting into software engineering, I realized how much these two fields have in common.  I see a team of engineers as a sort of musical group; playing, tinkering and even improvising to creatively and efficiently solve problems. </p>
 
             <p> As a musician, I&apos;ve learned to adapt to whatever a situation throws at me while collaborating with others to bring projects to life. I bring the same adaptability and team-oriented skill-set into software engineering where I strive to find creative and efficient solutions to complex problems. I&apos;m passionate about creating products that are accessible, innovative and impactful in our daily lives.</p>
 
