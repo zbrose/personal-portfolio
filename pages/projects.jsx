650d2d050d7e2ff2f2d0df7659f6859631b9c2a9
Zach Brose <97310057+zbrose@users.noreply.github.com>
2024-12-02 13:57:40 -0800

    fix icons

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -16,13 +16,13 @@ function Projects() {
                         <p>Whos Shows is a simple MERN stack application created for users to share concerts and events in one place.  I created this application because I wanted one location to find local events shared by friends of mine without the bloat of social media.  Instead of scrolling past an advertisement, a cat picture and a meme, my friends and I can find and upload shows with each other directly. </p>
                         <h4>Tech Used:</h4>
                         <div className="flex-box">
-                            <div className="img-wrapper"><Image src='/images/javascript.png' alt='the javascript logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/html5.png' alt='the html 5 logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/css3.png' alt='the css3 logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/node-js.png' alt='the node js logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/express.png' alt='the express logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/mongodb (1).png' alt='the mongodb logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/react.png' alt='the react logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/6js.png' alt='the javascript logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/bhtml5.png' alt='the html 5 logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/7css.png' alt='the css3 logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/9node-js.png' alt='the node js logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/aexpress.png' alt='the express logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/mongodb (1).png' alt='the mongodb logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/2react.png' alt='the react logo' width={50} height={50}/></div>
                         </div>
                         <div>
                             <a target='_blank' rel="noreferrer" className="link" href="https://62b64b4ff345050678b44cd2--friendly-trifle-312cf7.netlify.app">Deployed Project</a>
@@ -43,14 +43,14 @@ function Projects() {
                         <p>Party people is an app designed with the gathering in mind.  A MERN Stack appplication that allows users to create events as well as find and attend other users events.  Users can upload event details with images using cloudinary and use geolocation to find where an event is happening.</p>
                         <h4>Tech Used:</h4>
                         <div className="flex-box">
-                            <div className="img-wrapper"><Image src='/images/javascript.png' alt='the javascript logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/html5.png' alt='the html 5 logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/css3.png' alt='the css3 logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/bootstrap.png' alt='the bootstrap logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/node-js.png' alt='the node js logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/express.png' alt='the express logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/mongodb (1).png' alt='the mongodb logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/react.png' alt='the react logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/6js.png' alt='the javascript logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/bhtml5.png' alt='the html 5 logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/7css.png' alt='the css3 logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/bootstrap.png' alt='the bootstrap logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/9node-js.png' alt='the node js logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/aexpress.png' alt='the express logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/mongodb (1).png' alt='the mongodb logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/2react.png' alt='the react logo' width={50} height={50}/></div>
                         </div>
                         <div>
                             <a target='_blank' rel="noreferrer" className="link" href="https://party-people-app.netlify.app/">Deployed Project</a>
@@ -72,13 +72,13 @@ function Projects() {
                         <p>Side-Side is a music database and organization app in which a user can get information on recordings by artist name, record title, or song title using the Discogs Api. Users may view an albums information such as credits and track listing. Users are also able to bookmark and categorize records, adding them to a want-list or their growing collection.</p>
                         <h4>Tech Used:</h4>
                         <div className="flex-box">
-                            <div className="img-wrapper"><Image src='/images/javascript.png' alt='the javascript logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/html5.png' alt='the html 5 logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"> <Image src='/images/css3.png' alt='the css3 logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/bootstrap.png' alt='the bootstrap logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/node-js.png' alt='the node js logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/postgresql.png' alt='the postgres logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/express.png' alt='the express logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/6js.png' alt='the javascript logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/bhtml5.png' alt='the html 5 logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"> <Image src='/icons/7css.png' alt='the css3 logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/bootstrap.png' alt='the bootstrap logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/9node-js.png' alt='the node js logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/postgresql.png' alt='the postgres logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/aexpress.png' alt='the express logo' width={50} height={50}/></div>
                         </div>
                         <div>
                             <a target='_blank' rel="noreferrer" className="link" href="https://side-side.herokuapp.com/">Deployed Project</a>
@@ -98,9 +98,9 @@ function Projects() {
                         <p>Song Birds is an educational music game that helps you improve your ear for melodies. The player is able to control five birds, each one assigned a single musical note that sounds when you select a bird at the bottom of the screen. The object of the game is to listen to a playback of a melody and match the melody using your birds&apos; beautiful singing voices and your own ears.</p>
                         <h4>Tech Used:</h4>
                         <div className="flex-box">
-                            <div className="img-wrapper"><Image src='/images/javascript.png' alt='the javascript logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/html5.png' alt='the html 5 logo' width={50} height={50}/></div>
-                            <div className="img-wrapper"><Image src='/images/css3.png' alt='the css3 logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/6js.png' alt='the javascript logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/bhtml5.png' alt='the html 5 logo' width={50} height={50}/></div>
+                            <div className="img-wrapper"><Image src='/icons/7css.png' alt='the css3 logo' width={50} height={50}/></div>
                         </div>
                         <div>
                             <a target='_blank' rel="noreferrer" className="link" href="https://zbrose.github.io/song-birds/">Deployed Project</a>
